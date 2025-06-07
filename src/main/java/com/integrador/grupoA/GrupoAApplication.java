944de0d994d112337e1db9705a03edcf04cb2ae8
lucasandersen39 <lucas.andersen39@gmail.com>
2025-06-07 14:58:42 -0300

    Primer commit

---
+++ src/main/java/com/integrador/grupoA/GrupoAApplication.java
@@ -0,0 +1,13 @@
+package com.integrador.grupoA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupoAApplication.class, args);
+	}
+
+}
