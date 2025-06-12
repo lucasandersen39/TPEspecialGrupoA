de95bf6e85858ab1ba13c22a20625013d8e11b06
Joana Bugarin <Wooli3000@gmail.com>
2025-06-11 23:27:57 -0300

    se agrega microservicio Monopatin vacio

---
+++ msvc-monopatines/src/main/java/com/integrador/msvc_monopatines/MsvcMonopatinesApplication.java
@@ -0,0 +1,13 @@
+package com.integrador.msvc_monopatines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcMonopatinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcMonopatinesApplication.class, args);
+	}
+
+}
