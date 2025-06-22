7a004f361caca76be41cf7943906c60d2010ffcc
lucasandersen39 <lucas.andersen39@gmail.com>
2025-06-21 23:37:35 -0300

    Se agrego la clase GlobarErrorHandler para que maneje los estados de error

---
+++ api-gateway/src/main/java/com/integrador/api_gateway/handler/GlobalErrorHandler.java
@@ -0,0 +1,39 @@
+package com.integrador.api_gateway.handler;
+
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+
+@Component
+@Order(-2)
+public class GlobalErrorHandler implements ErrorWebExceptionHandler {
+
+    @Override
+    public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
+        String body = String.format("""
+            {
+              "error": "Ruta no válida",
+              "status": 404,
+              "timestamp": "%s"
+            }
+            """, Instant.now());
+
+        exchange.getResponse().setStatusCode(HttpStatus.NOT_FOUND);
+        exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
+
+        DataBuffer buffer = exchange.getResponse()
+                .bufferFactory()
+                .wrap(body.getBytes(StandardCharsets.UTF_8));
+
+        return exchange.getResponse().writeWith(Mono.just(buffer));
+    }
+}
+
