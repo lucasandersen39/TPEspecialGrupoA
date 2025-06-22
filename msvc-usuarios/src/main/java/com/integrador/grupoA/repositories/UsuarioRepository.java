981a098e8ffc739ceb49d043ee007cff9b722b21
Fede Cordeiro <federico.cordeiro95@gmail.com>
2025-06-22 15:02:15 -0300

    se agrega dockerfile, docker-compose y coleccion de Postman

--- msvc-usuarios/src/main/java/com/integrador/grupoA/repositories/UsuarioRepository.java
+++ msvc-usuarios/src/main/java/com/integrador/grupoA/repositories/UsuarioRepository.java
@@ -13,13 +13,13 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     @Query("SELECT new com.integrador.grupoA.services.dto.usuario.usuarioResponseDTO.UsuarioResponseDTO(" +
                   "u.nombre, u.apellido, u.email, u.telefono, u.tipoUsuario, u.nombreUsuario, u.dinero, u.fechaAlta, u.activo) " +
                   "FROM Usuario u WHERE u.nombreUsuario = :nombreUsuario")
-    Optional<UsuarioResponseDTO> findByNombreUsuario(@Param(":nombreUsuario") String nombreUsuario);
+    Optional<UsuarioResponseDTO> findByNombreUsuario(@Param("nombreUsuario") String nombreUsuario);
 
 
     @Query("SELECT new com.integrador.grupoA.services.dto.usuario.usuarioResponseDTO.UsuarioResponseDTO(" +
             "u.nombre, u.apellido, u.email, u.telefono, u.tipoUsuario, u.nombreUsuario, u.dinero, u.fechaAlta, u.activo) " +
             "FROM Usuario u WHERE u.email = :email")
-    Optional<UsuarioResponseDTO> findByEmail(@Param(":email") String email);
+    Optional<UsuarioResponseDTO> findByEmail(@Param("email") String email);
 
 
     @Query("SELECT new com.integrador.grupoA.services.dto.usuario.usuarioResponseDTO.UsuarioResponseDTO(" +
@@ -31,12 +31,12 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     @Query("SELECT new com.integrador.grupoA.services.dto.usuario.usuarioResponseDTO.UsuarioResponseDTO(" +
             "u.nombre, u.apellido, u.email, u.telefono, u.tipoUsuario, u.nombreUsuario, u.dinero, u.fechaAlta, u.activo) " +
             "FROM Usuario u WHERE u.id = :id")
-    Optional<UsuarioResponseDTO> buscarPorId(@Param(":id") Long id);
+    Optional<UsuarioResponseDTO> buscarPorId(@Param("id") Long id);
 
     //obtenerUsuariosPorTipo
     @Query("SELECT new com.integrador.grupoA.services.dto.usuario.usuarioResponseDTO.UsuarioResponseDTO(" +
             "u.nombre, u.apellido, u.email, u.telefono, u.tipoUsuario, u.nombreUsuario, u.dinero, u.fechaAlta, u.activo) " +
             "FROM Usuario u WHERE u.tipoUsuario = :tipoUsuario")
-    List<UsuarioResponseDTO> obtenerUsuariosPorTipo(@Param(":tipoUsuario") String tipoUsuario);
+    List<UsuarioResponseDTO> obtenerUsuariosPorTipo(@Param("tipoUsuario") String tipoUsuario);
 
 }
