9917c9d956d2aa0de5108edbca9af551054e2513
Fede Cordeiro <federico.cordeiro95@gmail.com>
2025-06-23 18:25:28 -0300

    se deja codigo comentado para crear un usuario admin

--- msvc-usuarios/src/main/java/com/integrador/grupoA/MsvcUsuariosApplication.java
+++ msvc-usuarios/src/main/java/com/integrador/grupoA/MsvcUsuariosApplication.java
@@ -1,7 +1,14 @@
 package com.integrador.grupoA;
 
+import com.integrador.grupoA.services.UsuarioService;
+import com.integrador.grupoA.services.dto.usuario.usuarioRequestDTO.UsuarioRequestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class MsvcUsuariosApplication {
@@ -9,5 +16,21 @@ public class MsvcUsuariosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MsvcUsuariosApplication.class, args);
 	}
+
+	/*
+	@Autowired
+	private UsuarioService usuarioService;
+
+	@Bean
+	CommandLineRunner init() {
+		return args -> {
+			if (usuarioService.buscarPorId(1L).isEmpty()) {
+				UsuarioRequestDTO defaultAdminUser = new UsuarioRequestDTO(
+						"admin", "admin", "admin@admin.com", "0800-admin", "admin"
+				);
+				usuarioService.crearUsuario(defaultAdminUser);
+			}
+		};
+	}*/
 
 }
