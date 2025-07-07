183944733268f77679803b4b095f6a103743a086
DylanKjolhede <kjolhededylan@gmail.com>
2025-07-07 18:23:01 -0300

    Añadido swagger msvc-paradas

--- msvc-paradas/src/main/java/com/integrador/grupoA/dto/ParadaMonopatinResponseDTO.java
+++ msvc-paradas/src/main/java/com/integrador/grupoA/dto/ParadaMonopatinResponseDTO.java
@@ -1,13 +1,20 @@
 package com.integrador.grupoA.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter @Setter
+@Getter
+@Setter
+@Schema(description = "Respuesta que contiene una parada y sus monopatines asociados")
 public class ParadaMonopatinResponseDTO {
+
+    @Schema(description = "Datos de la parada")
     private ParadaResponseDTO parada;
+
+    @Schema(description = "Lista de monopatines cercanos a la parada")
     private List<MonopatinResponseDTO> monopatines = new ArrayList<>();
 }
